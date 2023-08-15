38a20c43ee4e9ca595ecfd849a787b6d13bc1777
Ihor <132585512+IhorZolot@users.noreply.github.com>
2023-08-16 01:07:15 +0300

    final push2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy } from 'react';
+import React, { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import { Layout } from './Layout';
--- src/pages/Cast/Cast.jsx
+++ src/pages/Cast/Cast.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import PropTypes from 'prop-types';
 import { api } from 'servises/api';
 import { ActorCard, ActorImage, CastContainer } from './Cast.styled';
 
