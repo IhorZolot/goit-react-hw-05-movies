709ac1f681ac3ee228dd3d1d4e91757836240d65
Ihor <132585512+IhorZolot@users.noreply.github.com>
2023-08-16 21:33:06 +0300

    finaly 5

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { NotFoundStyled } from './NotFound.styled';
 
 export const NotFound = () => {
   return (
-    <div>
+    <NotFoundStyled>
       <h1>Page not found..</h1>
-    </div>
+      <NotFoundLinkStyled to="/">Go Home</NotFoundLinkStyled>
+    </NotFoundStyled>
   );
 };
+
+const NotFoundLinkStyled = styled(Link)`
+  margin-bottom: 20px;
+  text-decoration: none;
+  color: #460e57;
+  border-radius: 4px;
+  border: 2px solid #ccc;
+  padding: 5px 20px;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
